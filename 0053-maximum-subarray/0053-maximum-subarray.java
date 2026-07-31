feda7c7a82b4f3328a3913f550f4c95643c31e15
AmanPanchal <iamanpanchal@gmail.com>
2026-07-31 10:09:19 +0530

    Time: 1 ms (99.95%), Space: 77.5 MB (14.27%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -15,3 +15,26 @@ class Solution {
         return max_sum;
     }
 }
+
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+
+//         int currentSum = 0;
+//         int maxSum = nums[0];
+
+//         for (int num : nums) {
+
+//             currentSum += num;
+
+//             if (currentSum > maxSum) {
+//                 maxSum = currentSum;
+//             }
+
+//             if (currentSum < 0) {
+//                 currentSum = 0;
+//             }
+//         }
+
+//         return maxSum;
+//     }
+// }
