04603a495e8018a41ac792a5f25dad2a921bdd9e
AmanPanchal <iamanpanchal@gmail.com>
2026-08-01 22:35:41 +0530

    Time: 1894 ms (6.01%), Space: 56 MB (6.85%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,68 @@
+// 3. Boyer-Moore Voting Algorithm (Optimal)
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int candidate = nums[0];
+//         int count = 0;
+//         for(int i=0; i<nums.length; i++){
+//             if(count == 0){
+//                 candidate = nums[i];
+//             }
+//             if(nums[i] == candidate){
+//                 count++;
+//             }
+//             else{
+//                 count--;
+//             }
+//         }
+//     return candidate;
+
+//     }
+// }
+
+// 1. Brute Force Approach (O(n²))
 class Solution {
     public int majorityElement(int[] nums) {
-        int candidate = nums[0];
-        int count = 0;
-        for(int i=0; i<nums.length; i++){
-            if(count == 0){
-                candidate = nums[i];
+
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < n; j++) {
+
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
             }
-            if(nums[i] == candidate){
-                count++;
-            }
-            else{
-                count--;
+
+            if (count > n / 2) {
+                return nums[i];
             }
         }
-    return candidate;
 
+        return -1;
     }
 }
+
+// 2. HashMap Approach (O(n))
+
+// import java.util.HashMap;
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for (int num : nums) {
+
+//             map.put(num, map.getOrDefault(num, 0) + 1);
+
+//             if (map.get(num) > nums.length / 2) {
+//                 return num;
+//             }
+//         }
+
+//         return -1;
+//     }
+// }
