b4b7ddeed0b9ec82ffd26d38140b5878b53822b3
AmanPanchal <iamanpanchal@gmail.com>
2026-08-01 22:36:24 +0530

    Time: 2 ms (70.87%), Space: 55.6 MB (60.08%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,49 +1,49 @@
 // 3. Boyer-Moore Voting Algorithm (Optimal)
-// class Solution {
-//     public int majorityElement(int[] nums) {
-//         int candidate = nums[0];
-//         int count = 0;
-//         for(int i=0; i<nums.length; i++){
-//             if(count == 0){
-//                 candidate = nums[i];
-//             }
-//             if(nums[i] == candidate){
-//                 count++;
-//             }
-//             else{
-//                 count--;
-//             }
-//         }
-//     return candidate;
-
-//     }
-// }
-
-// 1. Brute Force Approach (O(n²))
 class Solution {
     public int majorityElement(int[] nums) {
-
-        int n = nums.length;
-
-        for (int i = 0; i < n; i++) {
-
-            int count = 0;
-
-            for (int j = 0; j < n; j++) {
-
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
+        int candidate = nums[0];
+        int count = 0;
+        for(int i=0; i<nums.length; i++){
+            if(count == 0){
+                candidate = nums[i];
             }
-
-            if (count > n / 2) {
-                return nums[i];
+            if(nums[i] == candidate){
+                count++;
+            }
+            else{
+                count--;
             }
         }
+    return candidate;
 
-        return -1;
     }
 }
+
+// 1. Brute Force Approach (O(n²))
+// class Solution {
+//     public int majorityElement(int[] nums) {
+
+//         int n = nums.length;
+
+//         for (int i = 0; i < n; i++) {
+
+//             int count = 0;
+
+//             for (int j = 0; j < n; j++) {
+
+//                 if (nums[i] == nums[j]) {
+//                     count++;
+//                 }
+//             }
+
+//             if (count > n / 2) {
+//                 return nums[i];
+//             }
+//         }
+
+//         return -1;
+//     }
+// }
 
 // 2. HashMap Approach (O(n))
 
