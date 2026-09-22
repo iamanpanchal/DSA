22786c23a2784253982f9c7bdb1e18b518647112
AmanPanchal <iamanpanchal@gmail.com>
2026-09-22 17:27:11 +0530

    Time: 21 ms (33.63%), Space: 91.7 MB (19.55%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -19,3 +19,42 @@ class Solution {
 }
 // Time  = O(n)
 // Space = O(n)
+
+// 1️⃣ Brute Force
+// for (int i = 0; i < nums.length; i++) {
+//     for (int j = i + 1; j < nums.length; j++) {
+//         if (nums[i] == nums[j]) {
+//             return nums[i];
+//         }
+//     }
+// }
+// Time  = O(n²)
+// Space = O(1)
+
+// 2️⃣ Sorting
+// Arrays.sort(nums);
+
+// for (int i = 1; i < nums.length; i++) {
+//     if (nums[i] == nums[i - 1]) {
+//         return nums[i];
+//     }
+// }
+
+// return -1;
+// Time  = O(n log n)
+// Space = O(1)* 
+
+// 3️⃣ HashSet
+// HashSet<Integer> set = new HashSet<>();
+
+// for (int num : nums) {
+//     if (set.contains(num)) {
+//         return num;
+//     }
+
+//     set.add(num);
+// }
+
+// return -1;
+// Time  = O(n)
+// Space = O(n)
