610dc1d229149cc3c5b8cbd826a65c0e5477e32a
AmanPanchal <iamanpanchal@gmail.com>
2026-09-25 15:03:30 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 13689-java-if-else-Hackerrank/13689-java-if-else.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        if(N%2 == 0){
+        scanner.close();
+    }
+        }
+            if(N <= 5){
+                System.out.println("Not Weird");
+            }
+        else{
+            System.out.println("Weird");
+        }
+            else if(N <= 20 ) {
+                System.out.println("Weird");
+            }   
+            else{
+                System.out.println("Not Weird");
+            }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
