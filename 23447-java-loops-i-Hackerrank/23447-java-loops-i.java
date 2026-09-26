e8a6c88dfe494495225f08727274d3ec2f49dbf0
AmanPanchal <iamanpanchal@gmail.com>
2026-09-26 15:16:56 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 23447-java-loops-i-Hackerrank/23447-java-loops-i.java
@@ -0,0 +1,28 @@
+import java.io.*;
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader
+        (System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        
+        bufferedReader.close();
+        for(int i=1; i<=10; i++){
+        
+            System.out.println(N + " x " + i + " = " + (N*i));
+        }
+import java.util.concurrent.*;
+
+
+import java.util.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+    }
+import java.util.regex.*;
+
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
